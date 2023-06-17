15e54d52209e27005b16682aba6f0dc85ff3fc73
yunchipang <yunchipang@gmail.com>
2023-06-16 21:58:40 -0700

    Refactor project, added Spring Data MongoDB dependency

---
+++ src/test/java/com/vocaboost/analyticsserver/AnalyticsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocaboost.analyticsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
