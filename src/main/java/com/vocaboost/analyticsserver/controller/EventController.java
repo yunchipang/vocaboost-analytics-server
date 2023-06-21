ac0dcf58ff343a3633c34f9f81b984f1c0b3a094
yunchipang <yunchipang@gmail.com>
2023-06-20 19:46:28 -0700

    Refactor endpoint to /event-tracking

--- src/main/java/com/vocaboost/analyticsserver/controller/EventController.java
+++ src/main/java/com/vocaboost/analyticsserver/controller/EventController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/event")
+@RequestMapping("/v1/event-tracking")
 public class EventController {
 
   @Autowired
